e4ffaa31bd1945f81138d4c45a21e252417270eb
Sal Espinosa <sespinos@gmail.com>
2018-09-01 12:07:56 -0600

    Check for cohort existence before create

--- app/models/census_cohort.rb
+++ app/models/census_cohort.rb
@@ -9,7 +9,8 @@ class CensusCohort
       nil
     else
       formatted = cohort_info.first.except("start_date", "status")
-      cohort = TuringCohort.create(formatted)
+      formatted["census_id"] = formatted.delete("id")
+      cohort = TuringCohort.where(census_id: formatted["census_id"]).first_or_create(formatted)
       CensusUser.create_from_census_cohort(cohort)
 
       cohort
