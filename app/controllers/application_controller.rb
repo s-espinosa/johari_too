a52eb2aec90ed6f690095acb9776f4ede6c4f6c0
Sal Espinosa <sespinos@gmail.com>
2018-09-01 16:13:58 -0600

    Add nav for logged in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :user_nav
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def user_dashboard
-    admin_dashboard_path if current_user.admin?
-    dashboard_path
+  def user_nav
+    if current_user.admin?
+      'shared/admin_nav'
+    else
+      'shared/default_nav'
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,11 @@ class SessionsController < ApplicationController
     census_user = CensusService.current_user(token)
     user        = CensusUser.create_from_census(census_user)
     session[:user_id] = user.id
-    redirect_to user_dashboard
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path
   end
 end
