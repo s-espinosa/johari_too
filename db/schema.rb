057904b6d93ed3977e390db8ee80836f21e45c19
Sal Espinosa <sespinos@gmail.com>
2018-09-03 19:23:38 -0600

    Add adjectives and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901044009) do
+ActiveRecord::Schema.define(version: 20180904011952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "adjectives", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "assignments", force: :cascade do |t|
     t.integer "giver_id"
