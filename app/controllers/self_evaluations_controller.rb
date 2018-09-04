8201d7cac956c6b07665e0137396bc5814f836c9
Sal Espinosa <sespinos@gmail.com>
2018-09-04 14:08:00 -0600

    Update self evaluation to only create on submission of attributes

--- app/controllers/self_evaluations_controller.rb
+++ app/controllers/self_evaluations_controller.rb
@@ -1,11 +1,12 @@
 class SelfEvaluationsController < ApplicationController
   def new
-    @assignment = Assignment.create(giver_id: current_user.id,
+    @assignment = Assignment.new(giver_id: current_user.id,
                                  receiver_id: current_user.id)
   end
 
   def create
-    assignment = Assignment.find(params[:assignment_id])
+    assignment = Assignment.create(giver_id: current_user.id,
+                                   receiver_id: current_user.id)
     Attribute.create_multiple(assignment, params[:adjectives])
     flash[:success] = "Successfully completed self-evaluation"
     redirect_to dashboard_path
